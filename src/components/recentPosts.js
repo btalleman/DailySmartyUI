f8ffa32651aa574caad5bfe62f2e4b400c1679ac
btalleman <btalleman@gmail.com>
2020-07-31 23:00:20 -0600

    created recnet posts and search bar components

---
+++ src/components/recentPosts.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class RecentPosts extends Component {
+	render() {
+		return (
+			<div className="recent-posts">
+				<div className="recent-posts_heading">Recent Posts</div>
+				<ul className="recent-posts_posts">
+					<li>recent post 0</li>
+					<li>recent post 1</li>
+					<li>recent post 2</li>
+				</ul>
+			</div>
+		);
+	}
+}
+
+export default RecentPosts;
